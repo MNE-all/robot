4dc7cb2a6f15e66ce6eff275666b317d285ca7c7
Николай Монашов <monashovnikolaye@gmail.com>
2022-09-07 12:25:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace robot
+{
+    class Program
+    {
+        static void Main()
+        {
+            int step = 0;
+            int left = Console.CursorLeft + 20, top = Console.CursorTop + 10;
+            Robot robot = new Robot();
+            char[] dir = { 'N', 'N', 'N', 'E', 'E', 'S', 'S', 'E', 'S', 'W', 'W' };
+            Console.SetCursorPosition(left, top);
+            Console.Write(step);
+            while (!robot.WhereBot(step) && step < dir.Length)
+            {
+                robot.Swipe(dir[step]);
+                switch (dir[step++])
+                {
+                    case 'N':
+                        Console.SetCursorPosition(left, --top);
+                        Console.Write($"{step}");
+                        break;
+                    case 'S':
+                        Console.SetCursorPosition(left, ++top);
+                        Console.Write($"{step}");
+                        break;
+                    case 'W':
+                        left -= 4;
+                        Console.SetCursorPosition(left, top);
+                        Console.Write($"{step}\t");
+                        break;
+                    case 'E':
+                        left += 4;
+                        Console.SetCursorPosition(left, top);
+                        Console.Write($"{step}\t");
+                        break;
+
+                }
+            }
+            Console.SetCursorPosition(20, 20);
+            if (!robot.WhereBot(step)) Console.WriteLine("Робот не вернулся на базу");
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ robot.cs
@@ -0,0 +1,40 @@
+﻿
+
+class Robot
+{
+    int x = 0, y = 0;
+
+    public void Swipe(char dir)
+    {
+        switch (dir)
+        {
+            case 'N':
+                y++;
+                break;
+            case 'S':
+                y--;
+                break;
+            case 'E':
+                x++;
+                break;
+            case 'W':
+                x--;
+                break;
+        }
+    }
+
+    public bool WhereBot(int step)
+    {
+        if (x == 0 && y == 0 && step != 0)
+        {
+            Console.SetCursorPosition(20, 20);
+            Console.WriteLine($"Робот вернуля за {step} шагов");
+            return true;
+        }
+        return false;
+    }
+
+
+    
+}
+
